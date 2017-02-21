dad80a1e8637ccbab7fd4fa7deebcaf59590dccb
Joseph Butler <jbutler@redhat.com>
2017-02-21 15:03:34 -0300

    Pass the OAuth2 token via JMSConnectionFactory.username

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This is an example of creating your own Security Plugin for ActiveMQ Artemis.
 
 As described in ActiveMQ Artemis documentation (https://activemq.apache.org/artemis/docs/1.5.2/security.html), there are a number of available plugins that come with Artemis.  In addition to those plugins, you can also create your own plugin and apply any custom logic.
 
-In this project, an OAuth2 Login Module is created to demonstrate this capability.
+This project is described in the following blog post:
+https://medium.com/@joelicious/extending-artemis-security-with-oauth2-7fd9b3dffe3#.fw0y2tt0y
 
 To use this module, build the project and then copy to ${ARTEMIS_HOME}/lib. It then can be invoked by updating the  ${BROKER_HOME}/etc/login.config file.
--- src/main/java/org/apache/activemq/artemis/plugins/security/jaas/OAuth2LoginModule.java
+++ src/main/java/org/apache/activemq/artemis/plugins/security/jaas/OAuth2LoginModule.java
@@ -1,6 +1,7 @@
 package org.apache.activemq.artemis.plugins.security.jaas;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
@@ -10,7 +11,10 @@ import java.util.Map;
 import java.util.Set;
 
 import javax.security.auth.Subject;
+import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
 import javax.security.auth.login.LoginException;
 import javax.security.auth.spi.LoginModule;
 
@@ -18,9 +22,9 @@ import org.apache.activemq.artemis.spi.core.security.jaas.RolePrincipal;
 import org.apache.activemq.artemis.spi.core.security.jaas.UserPrincipal;
 import org.jboss.logging.Logger;
 
-public class Oauth2LoginModule implements LoginModule {
+public class OAuth2LoginModule implements LoginModule {
 
-	private static final Logger logger = Logger.getLogger(Oauth2LoginModule.class);
+	private static final Logger logger = Logger.getLogger(OAuth2LoginModule.class);
 
 	private static final String OAUTH2_TOKEN = "org.apache.activemq.jaas.oauth2.token";
 	private static final String OAUTH2_ROLE = "org.apache.activemq.jaas.oauth2.role";
@@ -28,7 +32,7 @@ public class Oauth2LoginModule implements LoginModule {
 
 	private boolean debug;
 
-	private String userName = "oauth2-token";
+	private String userName;
 	private String roleName = "oauth2-role";
 	private String oauth2URL = "oauth2-url";
 
@@ -45,11 +49,9 @@ public class Oauth2LoginModule implements LoginModule {
 
 		this.subject = subject;
 		this.callbackHandler = callbackHandler;
+
 		debug = "true".equalsIgnoreCase((String) options.get("debug"));
 
-		if (options.get(OAUTH2_TOKEN) != null) {
-			userName = (String) options.get(OAUTH2_TOKEN);
-		}
 		if (options.get(OAUTH2_ROLE) != null) {
 			roleName = (String) options.get(OAUTH2_ROLE);
 		}
@@ -58,7 +60,7 @@ public class Oauth2LoginModule implements LoginModule {
 		}
 
 		if (debug) {
-			logger.debug("Initialized debug=" + debug + " guestUser=" + userName + " guestGroup=" + roleName + " url="
+			logger.debug("Initialized debug=" + debug + " guestGroup=" + roleName + " url="
 					+ oauth2URL);
 		}
 
@@ -67,6 +69,20 @@ public class Oauth2LoginModule implements LoginModule {
 	@Override
 	public boolean login() throws LoginException {
 		loginSucceeded = true;
+
+		Callback[] callbacks = new Callback[1];
+		callbacks[0] = new NameCallback("User name");
+
+		try {
+			callbackHandler.handle(callbacks);
+		} catch (IOException | UnsupportedCallbackException e) {
+			throw (LoginException) new LoginException().initCause(e);
+		}
+		
+		userName = ((NameCallback) callbacks[0]).getName();
+		
+		if (null == userName) 
+			loginSucceeded = false;
 
 		String newUrl = "https://" + oauth2URL + "?access_token=" + userName;
 		logger.debug("THis is the URL: " + newUrl);
@@ -121,7 +137,7 @@ public class Oauth2LoginModule implements LoginModule {
 
 		boolean returnableBoolean = true;
 		try {
-			
+
 			URL myURL = new URL(url);
 			URLConnection connection = myURL.openConnection();
 
@@ -131,19 +147,20 @@ public class Oauth2LoginModule implements LoginModule {
 			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
 				logger.debug("InputLine: " + inputLine);
-				
-				// Should not be executed if credentials are bad since 401 Exception is thrown.
+
+				// Should not be executed if credentials are bad since 401
+				// Exception is thrown.
 				// String matching login shown as example.
 				if (inputLine.contains("Bad credentials")) {
 					returnableBoolean = false;
 				}
 			}
 			in.close();
-			
+
 		} catch (Exception e) {
-			
+
 			// If token is bad, 401 Exception is caught here
-			
+
 			returnableBoolean = false;
 			logger.debug("Oauth2Exception: " + e.getMessage());
 		}
